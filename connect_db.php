a62bbb36ee4c46fc7c3e9323f683214e2545f238
Võ Huy Khang <47880923+huykhangvo@users.noreply.github.com>
2020-11-27 14:23:02 +0700

    Update connect_db.php

--- connect_db.php
+++ connect_db.php
@@ -1,8 +1,8 @@
 <?php
-$host = "localhost";
-$user = "root";
-$password = "mysql";
-$database = "ss1";
+$host = "sql3.freemysqlhosting.net";
+$user = "sql3378749";
+$password = "Q4AqyG3M93";
+$database = "sql3378749";
 $con = mysqli_connect($host, $user, $password, $database);
 if (mysqli_connect_errno()){
     echo "Connection Fail: ".mysqli_connect_errno();exit;
